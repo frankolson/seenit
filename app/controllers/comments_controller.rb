9caa6d72cbcc5ee59d603c8dc53e403eb13ad4b8
Will Olson <will.olson11@gmail.com>
2018-08-01 09:11:31 -0700

    feat: Update flash messages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,44 +1,30 @@
 class CommentsController < ApplicationController
-  before_action :authorized_user, only: [:edit, :update, :destroy]
-
-  # GET /comments/1/edit
-  def edit
-  end
+  before_action :authorized_user
 
   # POST /comments
-  # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
     @comment.link_id = params[:link_id]
     @comment.user = current_user
 
     if @comment.save
-      redirect_to @comment.link, notice: 'Comment was successfully created.'
+      flash[:success] = 'Comment was successfully created.'
+      redirect_to @comment.link
     else
-      redirect_back fallback_location: root_path,
-        alert: @comment.errors.full_messages.to_sentence
-    end
-  end
-
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
-  def update
-    if @comment.update(comment_params)
-      redirect_to @comment.link, notice: 'Comment was successfully updated.'
-    else
-      render :edit
+      flash[:error] = @comment.errors.full_messages.to_sentence
+      redirect_back fallback_location: root_path
     end
   end
 
   # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
     @comment.destroy
-    redirect_to comments_url, notice: 'Comment was successfully destroyed.'
+    flash[:success] = 'Comment was successfully destroyed.'
+    redirect_back fallback_location: root_url
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_comment
       @comment = current_user.comments.find_by(id: params[:id])
     end
@@ -47,12 +33,11 @@ class CommentsController < ApplicationController
       set_comment
 
       if @comment.nil?
-      redirect_back fallback_location: root_path,
-        notice: "Sorry, you can't edit someone else's comments"
+        flash[:error] = "Sorry, you can't edit someone else's comments."
+        redirect_back fallback_location: root_path
       end
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
       params.require(:comment).permit(:user_id, :link_id, :content)
     end
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,13 +4,11 @@ class LinksController < ApplicationController
   before_action :authorized_user, only: [:edit, :update, :destroy]
 
   # GET /links
-  # GET /links.json
   def index
     @links = Link.all
   end
 
   # GET /links/1
-  # GET /links/1.json
   def show
   end
 
@@ -24,57 +22,49 @@ class LinksController < ApplicationController
   end
 
   # POST /links
-  # POST /links.json
   def create
     @link = current_user.links.build(link_params)
 
-    respond_to do |format|
-      if @link.save
-        format.html { redirect_to @link, notice: 'Link was successfully created.' }
-        format.json { render :show, status: :created, location: @link }
-      else
-        format.html { render :new }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
-      end
+    if @link.save
+      flash[:success] = 'Link was successfully created.'
+      redirect_to @link
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /links/1
-  # PATCH/PUT /links/1.json
   def update
-    respond_to do |format|
-      if @link.update(link_params)
-        format.html { redirect_to @link, notice: 'Link was successfully updated.' }
-        format.json { render :show, status: :ok, location: @link }
-      else
-        format.html { render :edit }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
-      end
+    if @link.update(link_params)
+      flash[:success] = 'Link was successfully updated.'
+      redirect_to @link
+    else
+      render :edit
     end
   end
 
   # DELETE /links/1
-  # DELETE /links/1.json
   def destroy
     @link.destroy
-    respond_to do |format|
-      format.html { redirect_to links_url, notice: 'Link was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    flash[:success] = 'Link was successfully destroyed.'
+    redirect_to links_url
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_link
       @link = Link.find(params[:id])
     end
 
     def authorized_user
       @link = current_user.links.find_by(id: params[:id])
-      redirect_to links_path, notice: "Sorry, you can't edit someone else's links" if @link.nil?
+
+      if @link.nil?
+        flash[:error] = "Sorry, you can't edit someone else's links."
+        redirect_to links_path
+      end
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def link_params
       params.require(:link).permit(:title, :url)
     end
