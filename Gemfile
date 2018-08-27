b18a0e69ee573f08847410eebe3183321ab5a60a
Will Olson <will.olson11@gmail.com>
2018-08-26 18:16:52 -0700

    feat: Add basic seed data

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'devise', '~> 4.2'
 gem 'acts_as_votable', '~> 0.11'
+gem 'faker', '~> 1.6', '>= 1.6.6'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  email: 'will.olson11@gmail.com',
+  password: 'thisisgoingtochange',
+  password_confirmation: 'thisisgoingtochange',
+)
+
+# create users
+10.times.each do
+  password = Faker::Internet.password
+  User.create(
+    email: Faker::Internet.email,
+    password: password,
+    password_confirmation: password
+  )
+end
+
+# create links
+25.times.each do
+  link = Link.create(
+    url: Faker::Internet.url,
+    title: Faker::FamousLastWords.last_words,
+    user_id: User.order("RANDOM()").first.id
+  )
+
+  User.order("RANDOM()").sample(rand(10)).each do |user|
+    link.vote_by(voter: user, vote: ['like', 'bad'].sample)
+  end
+
+  rand(10).times do
+    comment = Comment.create(
+      user_id: User.order("RANDOM()").first.id,
+      content: Faker::GreekPhilosophers.quote
+    )
+
+    link.comments << comment
+
+    User.order("RANDOM()").sample(rand(10)).each do |user|
+      comment.vote_by(voter: user, vote: ['like', 'bad'].sample)
+    end
+  end
+end
